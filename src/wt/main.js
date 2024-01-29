d054fcf750de11ad838c1ef061a3d751124a90f5
Roshkovsky Petr <roshkovskypetr@gmail.com>
2024-01-30 00:51:53 +0100

    feat: add wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,19 @@
+import os from "os";
+
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCores = os.cpus();
+
+  cpuCores.forEach((_, index) => {
+    const myWorker = new Worker("./src/wt/worker.js", {
+      workerData: 10 + index,
+    });
+
+    myWorker.on("message", (result) => {
+      console.log(result);
+    });
+  });
 };
 
 await performCalculations();
