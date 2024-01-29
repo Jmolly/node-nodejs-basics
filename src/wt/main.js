b995d68a67eaf46dbe134684bc6393750d65d843
Roshkovsky Petr <roshkovskypetr@gmail.com>
2024-01-30 00:57:46 +0100

    feat: implement wt main

--- src/wt/main.js
+++ src/wt/main.js
@@ -2,18 +2,37 @@ import os from "os";
 
 import { Worker } from "worker_threads";
 
-const performCalculations = async () => {
-  const cpuCores = os.cpus();
-
-  cpuCores.forEach((_, index) => {
+const createWorker = (index) => {
+  return new Promise(function (resolve, reject) {
     const myWorker = new Worker("./src/wt/worker.js", {
       workerData: 10 + index,
     });
 
-    myWorker.on("message", (result) => {
-      console.log(result);
+    myWorker.on("message", (data) => {
+      resolve({ status: "resolved", data });
+    });
+
+    myWorker.on("error", () => {
+      reject({ status: "error", data: null });
     });
   });
+};
+
+const performCalculations = async () => {
+  const cpuCores = os.cpus();
+  const promises = [];
+
+  cpuCores.forEach((_, index) => {
+    promises.push(createWorker(index));
+  });
+
+  Promise.all(promises)
+    .then(function (data) {
+      console.log(data, "data");
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
 await performCalculations();
